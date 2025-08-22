7d586ec58fe4d06aa2be7271c96e821af1920f93
jibraelT <jthomas610@boyslatin.net>
2025-08-22 15:10:08 +0000

    submitted

---
+++ Unit1/DatatypeReview.py
@@ -0,0 +1,22 @@
+print('jibrael thomas')
+
+# print each of the folowing data types
+# string
+#boolean
+#integer 
+#float
+
+mylist = ["apple", "banana", "cherry"]
+print('mylist')
+print(10 > 9)
+x = 4
+print((x))
+z = 1.10
+x = -1.10
+print((x))
+print((z))
+
+#create and print's list that feature 2 of each data types.
+burgers = {"veggie","meat","plant based"}
+
+
